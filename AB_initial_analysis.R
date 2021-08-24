c208463341c3cd742cf9dcffb26247d070bb19c4
jenitivecase <jbrussow@gmail.com>
2021-08-24 15:57:43 -0400

    MIKAT scores; minor updates to other graphs. starting to work on product Qs

--- AB_initial_analysis.R
+++ AB_initial_analysis.R
@@ -38,8 +38,12 @@ joined_data <- pre_data %>%
                                 Link == 'http://packages.kognito.com/abt_tactics_2/' ~ 'General',
                                 Link == 'http://packages.kognito.com/abt_tactics_3/' ~ 'None')) %>%
   filter(!is.na(ExpGroups)) %>%
-  mutate(ExpGroups = factor(ExpGroups, levels = c("None", "General", "Explicit")))
+  mutate(ExpGroups = factor(ExpGroups, levels = c("None", "General", "Explicit"))) %>%
+  mutate(Q1001 = factor(Q1001, levels = c("Extremely negative", "Moderately negative", "Slightly negative",
+                                          "Neither positive nor negative",
+                                          "Slightly positive", "Moderately positive", "Extremely positive")))
 
+groups <- unique(joined_data$ExpGroups)
 
 ### SCORES ####
 score_means <- joined_data %>%
@@ -73,7 +77,7 @@ ggplot(joined_data) +
                                     label = round(MeanScore, 2)),
             nudge_x = 7) +
   scale_fill_manual(values = JB_palette[c(2,4,6)]) +
-  theme(legend.position = "bottom") +
+  theme(legend.position = "none") +
   labs(title = paste0("Score Distributions by ", exp_var_title),
        x = "Reported Score",
        y = "Participants",
@@ -106,8 +110,8 @@ NPS_boxes <- data.frame(category = c("Needs Improvement", "Good", "Great", "Exce
 
 
 ggplot(NPS_group_scores) +
-  geom_rect(data = NPS_boxes, mapping=aes(xmin=xmin, xmax=xmax, ymin = 0.2, ymax = 0.8, fill=category),
-            alpha = 0.8) +
+  geom_rect(data = NPS_boxes, mapping=aes(xmin=xmin, xmax=xmax, ymin = 0.25, ymax = 0.75, fill=category),
+            alpha = 0.75) +
   scale_y_continuous(limits = c(0, 1), breaks = NULL, minor_breaks = NULL) +
   facet_grid(ExpGroups ~ .) +
   geom_vline(aes(xintercept = NPS_score), size = 1) +
@@ -125,6 +129,7 @@ ggplot(NPS_group_scores) +
        x = "NPS Score",
        y = "",
        fill = "Category") 
+
 
 ggplot(joined_data) +
   geom_histogram(aes(x = Q1006, fill = Q1006_NPS_GROUP),
@@ -165,3 +170,178 @@ ggplot(joined_data, aes(x = Q44, y = Q1006)) +
        y = "NPS") +
   scale_y_continuous(limits = c(-0.5, 10.5), breaks = seq(0, 10, 2)) 
   
+
+### MIKAT ITEMS ####
+
+#recode to be scored variables
+#this code sucks and i'm sorry but at least it's clear, right?
+#... right?
+#so to be safe: the Q number == SOME VALUE is the important part
+#TRUE ~ 0 just means not that is wrong.
+joined_data <- joined_data %>%
+  mutate(Q32 = case_when(Q32 == FALSE ~ 1,
+                         TRUE ~ 0),
+         Q33 = case_when(Q33 == TRUE ~ 1,
+                         TRUE ~ 0),
+         Q34 = case_when(Q34 == TRUE ~ 1,
+                         TRUE ~ 0),
+         Q35 = case_when(Q35 == FALSE ~ 1,
+                         TRUE ~ 0),
+         Q36 = case_when(Q36 == TRUE ~ 1,
+                         TRUE ~ 0),
+         Q37.1 = case_when(Q37.1 == FALSE ~ 1,
+                         TRUE ~ 0)) %>%
+  mutate(Q1022 = case_when(Q1022 == FALSE ~ 1,
+                         TRUE ~ 0),
+         Q1023 = case_when(Q1023 == TRUE ~ 1,
+                         TRUE ~ 0),
+         Q1024 = case_when(Q1024 == TRUE ~ 1,
+                         TRUE ~ 0),
+         Q1025 = case_when(Q1025 == FALSE ~ 1,
+                         TRUE ~ 0),
+         Q1026 = case_when(Q1026 == TRUE ~ 1,
+                         TRUE ~ 0),
+         Q1027 = case_when(Q1027 == FALSE ~ 1,
+                           TRUE ~ 0)) %>%
+  mutate(MIKAT_pre_score = pmap_dbl(.l = list(Q32, Q33, Q34, Q35, Q36, Q37.1),
+                                    .f = sum)) %>%
+  mutate(MIKAT_post_score = pmap_dbl(.l = list(Q1022, Q1023, Q1024, Q1025, Q1026, Q1027),
+                                    .f = sum)) %>%
+  mutate(MIKAT_change = pmap_dbl(.l = list(MIKAT_post_score, MIKAT_pre_score),
+                                 .f = function(x, y){x-y}))
+
+MIKAT_ttest <- t.test(joined_data$MIKAT_pre_score, joined_data$MIKAT_post_score, paired = TRUE, alternative = "less")
+
+MIKAT_stat_subtitle <- ifelse(MIKAT_ttest$p.value <= 0.05,
+                             "Post-test scores were significantly higher, indicating knowledge gain",
+                             "Post-test scores were NOT significantly higher")
+  
+ggplot(joined_data) + 
+  geom_histogram(aes(x = MIKAT_pre_score, fill = JB_palette[1]),
+                 binwidth = 1, alpha = 0.5) +
+  geom_histogram(aes(x = MIKAT_post_score, fill = JB_palette[4]),
+                 binwidth = 1, alpha = 0.5) +
+  #label the posttest mean
+  geom_vline(aes(xintercept = mean(MIKAT_post_score)), size = 1, color = "black") +
+  geom_text(aes(x = mean(MIKAT_post_score), y = 100, 
+                label = paste0("Post mean:", round(mean(MIKAT_post_score), 2))),
+            nudge_x = .95, color = "black") +
+  #label the pretest mean
+  geom_vline(aes(xintercept = mean(MIKAT_pre_score)), size = 1, color = JB_palette[1]) +
+  geom_text(aes(x = mean(MIKAT_pre_score), y = 100, 
+                label = paste0("Pre mean:", round(mean(MIKAT_pre_score), 2))),
+            nudge_x = -.95, color = JB_palette[1]) +
+  scale_fill_identity(name = 'Pre/Post', guide = 'legend',labels = c('Post', 'Pre')) +
+  theme(legend.position = "none") + 
+  labs(title = paste0("Adapted MIKAT Pre/Post Scores for the full sample"),
+       subtitle = MIKAT_stat_subtitle,
+       caption = "According to a one-sided paired sample t-test",
+       x = "Adapted MIKAT Score",
+       y = "Participants") 
+
+
+MIKAT_grouped_means <- joined_data %>%
+  select(MIKAT_pre_score, MIKAT_post_score, ExpGroups) %>%
+  group_by(ExpGroups) %>%
+  summarise(MIKAT_pre_mean = mean(MIKAT_pre_score),
+            MIKAT_post_mean = mean(MIKAT_post_score)) %>%
+  mutate(MIKAT_change = MIKAT_post_mean - MIKAT_pre_mean)
+
+MIKAT_grouped_ttests <- vector("character", length(groups))
+for(group in 1:length(groups)){
+  temp <- t.test(filter(joined_data, ExpGroups == groups[group])$MIKAT_pre_score,
+                 filter(joined_data, ExpGroups == groups[group])$MIKAT_post_score,
+                 paired = TRUE, alternative = "less")
+  
+  if(temp$p.value <= 0.05) {
+    MIKAT_grouped_ttests[group] <- as.character(groups[group])
+  } 
+}
+
+
+if (all.equal(as.character(groups), MIKAT_grouped_ttests)){
+  MIKAT_grouped_subtitle <- "Post-test scores were significantly higher for all groups,\nindicating knowledge gain"
+} else if (all.equal(MIKAT_grouped_ttests, vector("character", length(groups)))){
+  MIKAT_grouped_subtitle <- "Post-test scores were NOT significantly higher for any of the groups"
+} else {
+  MIKAT_grouped_subtitle <- paste0("Post-test scores were significantly higher for these groups:",
+                                   paste0(MIKAT_grouped_ttests, collapse = ", "))
+}
+
+
+ggplot(joined_data) + 
+  geom_histogram(aes(x = MIKAT_pre_score, fill = JB_palette[1], group = ExpGroups),
+                 binwidth = 1, alpha = 0.5) +
+  geom_histogram(aes(x = MIKAT_post_score, fill = JB_palette[4], group = ExpGroups),
+                 binwidth = 1, alpha = 0.5) +
+  facet_grid(ExpGroups ~ .) +
+  #label the posttest mean
+  geom_vline(data = MIKAT_grouped_means, 
+             aes(xintercept = MIKAT_post_mean, group = ExpGroups), size = 1, color = "black") +
+  geom_text(data = MIKAT_grouped_means, 
+            aes(x = MIKAT_post_mean, group = ExpGroups, y = 100, 
+                label = paste0("Post mean:", round(MIKAT_post_mean, 2))),
+            nudge_x = .95, color = "black") +
+  #label the pretest mean
+  geom_vline(data = MIKAT_grouped_means, 
+             aes(xintercept = MIKAT_pre_mean, group = ExpGroups), size = 1, color = JB_palette[1]) +
+  geom_text(data = MIKAT_grouped_means, 
+            aes(x = MIKAT_pre_mean, group = ExpGroups, y = 100, 
+                label = paste0("Pre mean:", round(MIKAT_pre_mean, 2))),
+            nudge_x = -.95, color = JB_palette[1]) +
+  scale_fill_identity(name = 'Pre/Post', guide = 'legend',labels = c('Post', 'Pre')) +
+  theme(legend.position = "none") + 
+  labs(title = paste0("Adapted MIKAT Pre/Post Scores by ", exp_var_title),
+       subtitle = MIKAT_grouped_subtitle,
+       caption = "According to a one-sided paired sample t-test",
+       x = "Adapted MIKAT Score",
+       y = "Participants") 
+
+MIKAT_anova <- aov(MIKAT_change ~ ExpGroups, data = joined_data)
+Anova(MIKAT_anova)
+
+contrast1 = c(-2,1,1)
+contrast2 = c(0,-1,1)
+# contrast3 = c(2,-1,-1)
+# contrast4 = c(-1,2,-1)
+# contrast5 = c(-1,-1,2)
+
+contrasts(joined_data$ExpGroups) = cbind(contrast1, contrast2)
+
+MIKAT_anova <- aov(Q44 ~ ExpGroups, data = joined_data)
+summary.lm(MIKAT_anova)
+#same story with the results from the ANOVA
+
+
+### PRODUCT QUESTIONS ####
+
+label <- datadict[datadict$code == "Q1001", "longname"]
+
+question_means <- joined_data %>%
+  group_by(ExpGroups) %>%
+  summarise(n = n(),
+            MeanRating = mean(as.numeric(Q1001), na.rm = TRUE)) 
+
+
+ggplot(joined_data) +
+  geom_histogram(aes(x = as.integer(Q1001),
+                     fill = ExpGroups, group = ExpGroups),
+                 binwidth = 1) +
+  facet_grid(ExpGroups ~ .) +
+  scale_x_continuous(limits = c(0.5, 7.5), breaks = seq(1, 7, 1),
+                     minor_breaks = NULL, 
+                     labels = c("Extremely negative", "", "",
+                                "Neither positive nor negative",
+                                "", "", "Extremely positive")) +
+  geom_vline(data = question_means, aes(xintercept = MeanRating, group = ExpGroups)) +
+  geom_text(data = question_means, aes(x = MeanRating, y = 35, group = ExpGroups,
+                                    label = round(MeanRating, 2)),
+            nudge_x = 0.5) +
+  scale_fill_manual(values = JB_palette[c(2,4,6)]) +
+  theme(legend.position = "none") +
+  labs(title = paste0("Rating Distributions by ", exp_var_title),
+       subtitle = label,
+       x = "Rating",
+       y = "Participants",
+       fill = "Tactics Version") 
+
